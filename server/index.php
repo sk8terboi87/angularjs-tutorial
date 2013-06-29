f09f55830d02948aee23fe2d35f24c0f41ed9c15
Aravindan.J <aravindanj@ubuntu.(none)>
2013-06-29 17:47:07 +0530

    To checking custom data has arrived :D

--- server/index.php
+++ server/index.php
@@ -3,4 +3,9 @@ error_reporting(E_ALL | E_STRICT);
 header("Access-Control-Allow-Origin: *");
 require('UploadHandler.php');
 $upload_handler = new UploadHandler();
+/*
+HERE IS YOUR CUSTOM DATA
+$customData = json_decode($_POST['params'], true);
+print_r($customData);
+*/
 exit;
